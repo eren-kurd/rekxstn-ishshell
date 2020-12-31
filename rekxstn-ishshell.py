68dfe58220b86bb40e2b9fc0d087d6546866103e
hamaiosdev <69496990+hamaiosdev@users.noreply.github.com>
2020-12-31 21:34:22 +0300

    Add files via upload

---
+++ rekxstn-ishshell.py
@@ -0,0 +1,55 @@
+#!usr/bin/python2
+#coding=utf-8
+import os 
+import time as kat
+from os import system as HAMA
+
+
+hama = """
+\033[31m╭━━━┳━━━┳━╮╭━┳━━━╮
+\033[31m┃╭━╮┃╭━╮┃┃╰╯┃┃╭━╮┃
+\033[97m╰╯╭╯\033[33m┃┃╱┃┃╭╮╭╮\033[97m┃┃╱┃┃
+\033[97m╱╱┃╭\033[33m┫╰━╯┃┃┃┃┃\033[97m┃╰━╯┃
+\033[32m╱╱┃┃┃╭━╮┃┃┃┃┃┃╭━╮┃
+\033[32m╱╱╰╯╰╯╱╰┻╯╰╯╰┻╯╱╰╯
+\033[96m
+TB/Am Toola La layan </PRO • 7AMA> Drust Krawa bakar dey bo rekxstny Ish shellakat/n bo har keshak 👇🏻
+\033[93m========================================
+\033[92mTelegramProfile/xxxHAMAxxx
+^^^^^^^^^^^^^^^^^^^
+\033[92mDiscord.Server/https://discord.gg/TK58aqRYCU
+\033[93m========================================
+"""
+print hama
+kat.sleep(1)
+HAMA('wget -qO- http://dl-cdn.alpinelinux.org/alpine/v3.12/main/x86/apk-tools-static-2.10.5-r1.apk | tar -xz sbin/apk.static && ./sbin/apk.static add apk-tools && rm sbin/apk.static && rmdir sbin 2> /dev/null ; python2 -m ensurepip ; pip install —upgrade pip ; pip2 install requests mechanize ; pip2 install python')
+print'\033[33m==\033[95m16%'
+kat.sleep(1)
+HAMA('apk update ; apk upgrade ; apk add python ; apk add python2 ; apk add ruby ; apk add git ; apk add php ; apk add perl ; apk add nmap ; apk add bash ; apk add clang ; apk add macchanger')
+print'\033[33m===\033[96m36%'
+kat.sleep(1)
+HAMA('apk add nano ; apk add figlet ; apk add cowsay ; apk add curl ; apk add tar')
+print'\033[33m====\033[92m47%'
+kat.sleep(1)
+HAMA('apk add zip ; apk add unzip ; apk add tor ; apk add sudo ; apk add wget')
+print'\033[33m=====\033[94m59%'
+kat.sleep(1)
+HAMA('apk add wcalc ; apk add openssl ; apk add bmon')
+print'\033[33m======\033[91m69%'
+kat.sleep(1)
+HAMA('apk add github ; apk add openssh')
+print'\033[33m=======\033[93m84%'
+
+HAMA('apk add g++ ; apk add gcc')
+print'\033[33m========\033[95m96%'
+HAMA('apk add python3 ; apk add py-pip')
+print'\033[33m=========\033[96m100%'
+HAMA('cd .. ; mkdir HAMA ; cd HAMA ; touch AM ISHSHELL REKXRA LALAYAN </PRO • 7AMA>')
+print"""
+\033[91md8888b.  .d88b.  d8b   db d88888b 
+\033[31m88  `8D .8P  Y8. 888o  88 88'     
+\033[94m88   88 88    88 88V8o 88 88ooooo 
+\033[96m88   88 88    88 88 V8o88 88~~~~~ 
+\033[92m88  .8D `8b  d8' 88  V888 88.     
+\033[32mY8888D'  `Y88P'  VP   V8P Y88888P """
+
